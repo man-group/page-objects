bb50bc7dd8be0776835c0c1a306bf8f9777bfe63
Edward Easton <edward@last.fm>
2014-09-30 12:59:03 +0100

    Bumped version, updated change log

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,6 +3,12 @@
 Release History
 ---------------
 
+1.0.1 (2014-09-30)
+++++++++++++++++++
+
+- Added PageObject.get(uri) method, based off of the page's root_uri attribute.
+
+
 1.0.0 (2014-09-29)
 ++++++++++++++++++
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = ['selenium']
 
 setup(
     name='page-objects',
-    version='1.0.0',
+    version='1.0.1',
     description='Page Objects for Python',
     long_description=readme + '\n\n' + changes,
     author='Edward Easton',
